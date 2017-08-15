d1d1c63b8a5ca16ce0b24216122a3fe641e7108f
ly.ho <ly.ho@asiantech.vn>
2017-08-15 16:11:44 +0700

    add permission

--- app/src/main/java/com/bkdev/translation/ui/welcome/LoginActivity.java
+++ app/src/main/java/com/bkdev/translation/ui/welcome/LoginActivity.java
@@ -1,7 +1,11 @@
 package com.bkdev.translation.ui.welcome;
 
+import android.Manifest;
+import android.support.annotation.NonNull;
+
 import com.bkdev.translation.R;
 import com.bkdev.translation.ui.BaseActivity;
+import com.bkdev.translation.util.PermissionUtil;
 
 import org.androidannotations.annotations.EActivity;
 
@@ -12,6 +16,11 @@ import org.androidannotations.annotations.EActivity;
 public class LoginActivity extends BaseActivity {
     @Override
     protected void init() {
+        PermissionUtil.onCheckPermission(this, 3, Manifest.permission.READ_CONTACTS);
+    }
 
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
     }
 }
